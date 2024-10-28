4e1456969656d579eb5680e929cdb5a30355201f
Tobias Molland <112409957+ToBeAss@users.noreply.github.com>
2024-10-28 17:50:04 +0100

    Testing auto_pull

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import scripts.timeout as timeout
 import aloe.main as aloe
+import scripts.discord as discord
+import data.webhooks as webhooks
 
 def main():
+    # Test function
+    discord.send_to_discord(webhooks.aloe, "Hello World!")
     while True:
         # Wait until next whole hour before running the rest of the program
         timeout.next_hour()
