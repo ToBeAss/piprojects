c33cc2f25f6f6609655e3438339dbf65fbfda9dd
Tobias Molland <tobiasmol@uia.no>
2024-11-06 12:26:45 +0100

    Added system message when rebooted

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ import scripts.discord as discord
 import my_secrets.webhooks as webhooks
 
 def main():
-    # Test function
-    discord.send_to_discord(webhooks.aloe, "rebooting")
+    # When the Raspberry Pi is rebooted, it should automatically start the main script an display this message:
+    discord.send_to_discord(webhooks.aloe, "System rebooted")
+    
     while True:
         # Wait until next whole hour before running the rest of the program
         timeout.next_hour()
