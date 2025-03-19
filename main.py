7a195ef8c381d9dc52c83011cda7efbe0ba85c2b
Tobias Molland <tobiasmol@uia.no>
2025-03-19 16:29:29 +0100

    fix: issue where sensor data was never sent to discord

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import datetime, time
 import src.timeout as timeout
 import src.discord as discord
 import my_secrets.webhooks as webhooks
@@ -7,32 +8,26 @@ import aloe.message as message
 
 def main():
     # When the Raspberry Pi is rebooted, it should automatically start the main script an display this message:
-    discord.send_to_discord(webhooks.aloe, "System rebooted")
-
-    temp = [] # Temporary readings deleted every hour
+    discord.send_to_discord(webhooks.piprojects, "System rebooted")
+    temp = []
     
     while True:
-        # Wait until next whole hour before running the rest of the program
-        next_hour = timeout.next_hour()
+        # Always sleep for a minute
         next_min = timeout.next_min()
+        timeout.sleep(next_min)
 
-        if (next_hour <= next_min):
-            # Calculate a summary every hour and send the result to Discord
-            timeout.sleep(next_hour)
+        # Check if it's a new hour
+        if datetime.now().minute == 0:
             summary = sensor.get_summary(temp)
             sensor.store_summary(summary)
             result = summary["Median Moisture(%)"]
             content = message.create_message(result)
             discord.send_to_discord(webhooks.aloe, content)
-            temp = []
-            data = sensor.get_data()
-            temp.append(data["Moisture"])
-            sensor.store_data(data)
-        else:
-            # Read sensor data every minute
-            timeout.sleep(next_min)
-            data = sensor.get_data()
-            temp.append(data["Moisture"])
-            sensor.store_data(data)
+            temp = []  # Temporary readings deleted every hour
+
+        # Collect data every minute
+        data = sensor.get_data()
+        temp.append(data["Moisture"])
+        sensor.store_data(data)
 
 main()
